7bfac69c061f70fed4db80ead4e346d1fd18d1eb
yanisboidin <yanisboidin@laposte.net>
2026-05-05 13:17:14 +0200

    fix: config for ci/cd

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -1,9 +1,30 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  host: process.env.POSTGRES_HOST ?? 'localhost',
-  port: parseInt(process.env.POSTGRES_PORT ?? '5432', 10),
-  username: process.env.POSTGRES_USER ?? 'hotel_user',
-  password: process.env.POSTGRES_PASSWORD ?? 'hotel_pass',
-  name: process.env.POSTGRES_DB ?? 'hotel_db',
+  host:
+    process.env.DATABASE_HOST ||
+    process.env.POSTGRES_HOST ||
+    'postgres',
+
+  port: parseInt(
+    process.env.DATABASE_PORT ||
+      process.env.POSTGRES_PORT ||
+      '5432',
+    10,
+  ),
+
+  username:
+    process.env.DATABASE_USER ||
+    process.env.POSTGRES_USER ||
+    'hotel_user',
+
+  password:
+    process.env.DATABASE_PASSWORD ||
+    process.env.POSTGRES_PASSWORD ||
+    'hotel_pass',
+
+  name:
+    process.env.DATABASE_NAME ||
+    process.env.POSTGRES_DB ||
+    'hotel_db',
 }));
